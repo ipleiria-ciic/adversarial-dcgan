9523c295f3467c79dec1b1e929c4fc496dbd6ad8
José Areia <jose.apareia@gmail.com>
2025-03-06 18:47:36 +0000

    refactor: add new loop for each delta

--- run.sh
+++ run.sh
@@ -2,20 +2,21 @@
 
 ATTACK=FGSM
 MODEL=AlexNet
-EPOCHS=10000
-BATCH=64
-EPOCH_TO_RESUME=5000
+EPOCHS=60
+BATCH=128
+EPOCH_TO_RESUME=0
+DELTAS="0.01 0.05 0.10 0.15 0.20"
 
-# Workflow Order: Attacks -> DCGAN -> Export -> Evaluation
+for DELTA in $DELTAS; do
+    # 1. Attacks (Perturbation)
+    python Attacks/$ATTACK.py --model $MODEL --delta $DELTA
 
-# 1. Attacks (Perturbation)
-python Attacks/$ATTACK.py --model $MODEL
+    # 2. DCGAN
+    python DCGAN/main.py --attack $ATTACK --epochs $EPOCHS --batch $BATCH --resume $EPOCH_TO_RESUME --delta $DELTA
 
-# 2. DCGAN
-python DCGAN/DCGAN.py --attack $ATTACK --epochs $EPOCHS --batch $BATCH --resume $EPOCH_TO_RESUME
+    # 3. Export
+    # python DCGAN/export-images.py --attack $ATTACK --epochs $EPOCHS --batch $BATCH
 
-# 3. Export
-python DCGAN/Export.py --attack $ATTACK --epochs $EPOCHS --batch $BATCH
-
-# 4. Evaluation
-python Evaluation/Evaluation.py --attack $ATTACK --model $MODEL
+    # 4. Evaluation
+    # python Evaluation/Evaluation.py --attack $ATTACK --model $MODEL
+done
