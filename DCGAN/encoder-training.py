9c934a43ce36414e752c02a4f18a824ab0f5b07a
José Areia <jose.apareia@gmail.com>
2025-03-06 18:49:15 +0000

    feat: add new array to store losses

--- DCGAN/encoder-training.py
+++ DCGAN/encoder-training.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--attack", type=str)
+parser.add_argument("--checkpoint_epochs", type=int)
 parser.add_argument("--epochs", type=int)
 parser.add_argument("--batch_size", type=int)
 args = parser.parse_args()
@@ -31,7 +32,7 @@ netE = utils.Encoder(feature_map_e, channel_number, latent_vector_z).to(device)
 netG = utils.Generator(gpu_number, feature_map_g, channel_number, latent_vector_z).to(device)
 netG.apply(utils.weights_init)
 
-checkpoint = torch.load(f"DCGAN/{args.attack}/Models/Checkpoint-{args.attack}-Epoch-{num_epochs}-{batch_size}.pth", weights_only=True)
+checkpoint = torch.load(f"DCGAN/{args.attack}/Models/Best-Checkpoint-{args.attack}-Epoch-{args.checkpoint_epochs}-{batch_size}.pth", weights_only=True)
 netG.load_state_dict(checkpoint["netG_state_dict"])
 netG.to(device)
 netG.eval()
@@ -41,7 +42,9 @@ criterion = nn.MSELoss()
 # vgg_loss_fn = utils.VGGPerceptualLoss(layer=8).to(device)
 optimizerE = optim.Adam(netE.parameters(), lr=adam_lr, betas=(adam_beta, 0.999))
 
-num_epochs = 100
+encoder_loss = []
+best_loss = float('inf')
+
 for epoch in range(num_epochs):
     for i, (real_images, _) in enumerate(dataloader):
         real_images = real_images.to(device)
@@ -58,6 +61,18 @@ for epoch in range(num_epochs):
         optimizerE.step()
 
         if i % 100 == 0:
-            print(f"[Epoch {epoch}/{num_epochs}] [Batch {i}/{len(dataloader)}] Loss: {loss.item():.4f}")
+            print(f"[Epoch {epoch}/{num_epochs}][Batch {i:03}/{len(dataloader)}] - Loss: {loss.item():.4f}")
+        
+        encoder_loss.append(loss.item())
+
+        if loss.item() < best_loss:
+            best_loss = loss.item()
+            torch.save({
+                'epoch': epoch,
+                'netE_state_dict': netE.state_dict(),
+                'optimizerE_state_dict': optimizerE.state_dict(),
+                'best_loss': best_loss,
+                'encoder_losses': encoder_loss
+            }, f"Best-Checkpoint-Encoder-{batch_size}.pth")
 
 torch.save(netE.state_dict(), f"DCGAN/{args.attack}/Models/Encoder-Final.pth")
